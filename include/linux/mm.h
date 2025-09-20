527383df4b1b802c14f611785c3badfc56c420b5
bengris32 <bengris32@protonmail.ch>
2025-09-20 01:28:25 +0100

    Revert "[ALPS04983389] FROMLIST: mm: Add a speculative page fault switch in sysctl"
    This reverts commit 80837a143b53cd83349a69e3cb66e55b48e88590.
    
    Change-Id: Id77c5f38227763c7567e9b3c174ab2626275316c
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1512,7 +1512,6 @@ extern vm_fault_t handle_mm_fault(struct vm_area_struct *vma,
 			unsigned long address, unsigned int flags);
 
 #ifdef CONFIG_SPECULATIVE_PAGE_FAULT
-extern int sysctl_speculative_page_fault;
 extern vm_fault_t __handle_speculative_fault(struct mm_struct *mm,
 				      unsigned long address,
 				      unsigned int flags);
@@ -1520,8 +1519,6 @@ static inline vm_fault_t handle_speculative_fault(struct mm_struct *mm,
 					   unsigned long address,
 					   unsigned int flags)
 {
-	if (unlikely(!sysctl_speculative_page_fault))
-		return VM_FAULT_RETRY;
 	/*
 	 * Try speculative page fault for multithreaded user space task only.
 	 */
